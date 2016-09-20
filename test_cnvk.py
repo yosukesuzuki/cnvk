e918962864f40a0e6e8051d5f85b0d50c491bbdc
Yosuke Suzuki <yosuke.suzuki@gmail.com>
2016-09-21 00:05:34 +0900

    add test for converting alphabet and digit

--- test_cnvk.py
+++ test_cnvk.py
@@ -5,5 +5,7 @@ import cnvk
 from nose.tools import *
 
 
-def test_zen_to_han_alpha():
+def test_zen_to_han():
     eq_(u'abcdefghijklmnopqrstuvwxyz', cnvk.convert(u'ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ', cnvk.H_ALPHA))
+    eq_(u'abcdefghijklmnopqrstuvwxyz1234567890', cnvk.convert(u'ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ１２３４５６７８９０',
+        cnvk.H_ALPHA, cnvk.H_NUM))
